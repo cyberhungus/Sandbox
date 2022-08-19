486f0d435cdf1f7d8d6e1848fd32f704907ea614
moritz.menzel <moritz.menzel@student.fh-kiel.de>
2022-08-19 16:53:19 +0200

    different colours in output, upscaling

--- Data_Interpreter.py
+++ Data_Interpreter.py
@@ -11,7 +11,8 @@ class Data_Interpreter(Thread):
     def __init__(self,input_queue, output_queue):
         self.input = input_queue
         self.output = output_queue
-        self.waterlevel = 210  
+        self.waterlevel = 230
+
                 
 
     def run(self):
@@ -19,7 +20,9 @@ class Data_Interpreter(Thread):
             if not self.input.empty():
                 new_data = self.input.get_nowait()
                 processed_data = self.interpret_data(new_data)
+                processed_data = cv2.resize(processed_data,dsize=None,fx=1.5,fy=1.5)
                 self.output.put_nowait(processed_data)
+
 
     def interpret_data(self, data):
         #make the grayscale data into a rgb data array and apply filters
@@ -30,8 +33,8 @@ class Data_Interpreter(Thread):
     
     #this function does some neccessary conversions, putting the 
     def do_pretty_rgb(self, data):
-        pretty_array = fc.pretty_depth_cv(data)
-        img = Image.fromarray(pretty_array,"L").convert("RGB")
+        pretty_img = fc.pretty_depth_cv(data)
+        img = Image.fromarray(pretty_img,"L").convert("RGB")
         #.filter(ImageFilter.EDGE_ENHANCE)
         rgb_array = np.array(img)
         return rgb_array
@@ -39,5 +42,27 @@ class Data_Interpreter(Thread):
     def height_calc_new(self,data):
 
         data[np.where((data>=[self.waterlevel,self.waterlevel,self.waterlevel]).all(axis=2))] = [250,0,0]
-        data[np.where((data<=[self.waterlevel,self.waterlevel,self.waterlevel]).all(axis=2))] = [0,self.waterlevel,0]
+        #data[np.where((data<=[self.waterlevel+10,self.waterlevel+10,self.waterlevel+10]).all(axis=2))] =[150,self.waterlevel,]
+        #data[np.where((data<=[self.waterlevel,self.waterlevel,self.waterlevel]).all(axis=2))] =[0,self.waterlevel,0]
+        #data[(np.where((data >= [self.waterlevel+10,self.waterlevel+10,self.waterlevel+10]).all(axis=2) & (data <= [self.waterlevel+20,self.waterlevel+20,self.waterlevel+20]).all(axis=2)))]=[50,self.waterlevel,60]
+        for num in range (170,self.waterlevel):
+            #data[np.all(data == [num,num,num], axis=-1)] = (0,num,0)
+            t = Thread(target=self.pixel_transform_worker,args =(data, num, ))
+            t.start()
+
         return data
+        
+    def pixel_transform_worker(self, data, num):
+        if num > self.waterlevel-10:
+            data[np.all(data == [num,num,num], axis=-1)] = (0,0,num+20)
+
+        elif num > self.waterlevel - 20:
+            data[np.all(data == [num,num,num], axis=-1)] = (0,num,num+30) 
+        
+        elif num > self.waterlevel - 30:
+            data[np.all(data == [num,num,num], axis=-1)] = (0,num,num+40) 
+        
+        elif num > self.waterlevel - 40:
+            data[np.all(data == [num,num,num], axis=-1)] = (0,num,num+50) 
+
+        data[np.all(data == [num,num,num], axis=-1)] = (0,num,0)
