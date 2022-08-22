56bb91a52bb2bfc7fcbb6b883c84bf2e59ed936e
Karl Hungus <vhkchvuoudgcwwqkxk@miucce.online>
2022-08-22 17:06:18 +0200

    added control window

--- Data_Visualizer.py
+++ Data_Visualizer.py
@@ -16,9 +16,12 @@ class Data_Visualizer:
                 self.current_image = self.input.get_nowait()
                 #print("DATA VIS RECEIVED:",self.current_image)
                 try:
-
+                    cv2.namedWindow("OUTPUT", cv2.WND_PROP_FULLSCREEN)
+                    cv2.setWindowProperty("OUTPUT",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+                    cv2.namedWindow("OUTPUT_CONTROL")
+                    cv2.moveWindow("OUTPUT_CONTROL", 100,100)
                     cv2.imshow("OUTPUT",self.get_current())
-                    cv2.setWindowProperty("OUTPUT", cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+                    cv2.imshow("OUTPUT_CONTROL",self.get_current())
                 except Exception as ex:
                     print("Error in Visualizer: ", ex)
                 if cv2.waitKey(10) == 27:
