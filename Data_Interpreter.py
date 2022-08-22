4028b9179da82214b40b6c4e5faa0f58a66695da
Karl Hungus <vhkchvuoudgcwwqkxk@miucce.online>
2022-08-22 16:56:14 +0200

    removed unneeded imports

--- Data_Interpreter.py
+++ Data_Interpreter.py
@@ -1,6 +1,3 @@
-from cgi import test
-from dis import pretty_flags
-from distutils.fancy_getopt import FancyGetopt
 from threading import Thread 
 from multiprocessing import Queue
 import frame_convert2 as fc
